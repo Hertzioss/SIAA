23231b8a86607069b184417952ce6de58f2fc49f
Luis Herazo <h2toxic@gmail.com>
2026-02-13 07:13:13 -0400

    feat: Add Content-Security-Policy header configuration to Next.js.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -15,7 +15,19 @@ const nextConfig: NextConfig = {
         pathname: "/storage/v1/object/public/**",
       },
     ],
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://escritorio.legal; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://desarrollo-siaa-db.jnvzha.easypanel.host https://data.escritorio.legal; font-src 'self' data:; connect-src 'self' https://data.escritorio.legal https://ve.dolarapi.com https://desarrollo-siaa-db.jnvzha.easypanel.host;",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
