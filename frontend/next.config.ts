6f3c043790a46e3f46dfc181cd494850bb56d955
Luis Herazo <h2toxic@gmail.com>
2026-02-13 07:20:05 -0400

    chore: Remove Content-Security-Policy headers configuration from Next.js config.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,18 +16,5 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://escritorio.legal; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://desarrollo-siaa-db.jnvzha.easypanel.host https://data.escritorio.legal; font-src 'self' data:; connect-src 'self' https://data.escritorio.legal https://ve.dolarapi.com https://desarrollo-siaa-db.jnvzha.easypanel.host;",
-          },
-        ],
-      },
-    ];
-  },
 };
 export default nextConfig;
